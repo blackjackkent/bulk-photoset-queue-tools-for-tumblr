6ab2a38adaae3f3f3d762cc6aab7379583eaef8f
Rosalind W <blackjacksoftwaredev@gmail.com>
2022-01-31 00:44:08 -0600

    Most formatting buttons fixed

--- src/utilities/appendRichButtons.js
+++ src/utilities/appendRichButtons.js
@@ -3,9 +3,13 @@ import butt from './butt';
 const appendRichButtons = (pbe, rich, brick) => {
 	// pbe meanse photo brick edit, but I recycled this
 	// to make buttons for the caption adder thing too :)
-	const addTextFormatting = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
+	const addTextFormatting = (event, element) => {
+		console.log({
+			event,
+			element
+		});
+		event.preventDefault();
+		event.stopPropagation();
 		const sel = window.getSelection();
 		if (typeof sel.rangeCount !== 'undefined' && sel.rangeCount === 0) {
 			return;
@@ -16,29 +20,23 @@ const appendRichButtons = (pbe, rich, brick) => {
 		let ancestor;
 		let ancClone;
 		let middle;
-		const isNode = this.getAttribute('data-node') !== 'none';
+		const isNode = element.getAttribute('data-node') !== 'none';
 		if (isNode) {
-			nod = document.createElement(this.getAttribute('data-node'));
+			nod = document.createElement(element.getAttribute('data-node'));
 			nod.appendChild(document.createTextNode(sel.toString()));
-			if (this.getAttribute('data-class') !== 'none') {
-				nod.classList.add(this.getAttribute('data-class'));
+			if (element.getAttribute('data-class') !== 'none') {
+				nod.classList.add(element.getAttribute('data-class'));
 			}
-			if (
-				this.getAttribute('data-node') === 'a' &&
-				this.getAttribute('data-attr') === 'href'
-			) {
-				nod.href =
-					pbe.getElementsByClassName('a_button_input')[0].value;
+			if (element.getAttribute('data-node') === 'a' && element.getAttribute('data-attr') === 'href') {
+				nod.href = pbe.getElementsByClassName('a_button_input')[0].value;
 			}
 		} else {
 			nod = document.createTextNode(sel.toString());
 		}
 		rng.deleteContents();
 		// data-parent is like ul li || ol li...
-		if (this.getAttribute('data-parent') !== 'none') {
-			const pnt = document.createElement(
-				this.getAttribute('data-parent')
-			);
+		if (element.getAttribute('data-parent') !== 'none') {
+			const pnt = document.createElement(element.getAttribute('data-parent'));
 			pnt.appendChild(nod);
 			rng.insertNode(pnt);
 		} else {
@@ -51,10 +49,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 				ancClone = ancestor.cloneNode(true);
 				middle = document.createDocumentFragment();
 				middle.appendChild(document.createTextNode(sel.toString()));
-				ancestor.innerText = ancestor.innerText.substring(
-					0,
-					sel.anchorOffset
-				);
+				ancestor.innerText = ancestor.innerText.substring(0, sel.anchorOffset);
 				ancClone.innerText = ancClone.innerText.substring(
 					sel.anchorOffset + sel.toString().length,
 					ancClone.innerText.length
@@ -66,10 +61,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 				if (ancClone.innerHTML.length > 0) {
 					frag.appendChild(ancClone);
 				}
-				rng.commonAncestorContainer.parentNode.replaceChild(
-					frag,
-					rng.commonAncestorContainer
-				);
+				rng.commonAncestorContainer.parentNode.replaceChild(frag, rng.commonAncestorContainer);
 			}
 		}
 		brick.classList.remove('visible-colors');
@@ -84,7 +76,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	xb.setAttribute('data-parent', 'none');
 	xb.removeAttribute('id');
 	xb.setAttribute('title', 'Remove Formatting');
-	xb.addEventListener('click', addTextFormatting);
+	xb.addEventListener('click', (e) => addTextFormatting(e, xb));
 	const hb = butt('H');
 	hb.classList.add(hb.id);
 	hb.setAttribute('data-node', 'h2');
@@ -93,7 +85,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	hb.setAttribute('data-parent', 'none');
 	hb.removeAttribute('id');
 	hb.setAttribute('title', 'Heading');
-	hb.addEventListener('click', addTextFormatting);
+	hb.addEventListener('click', (e) => addTextFormatting(e, hb));
 	const bb = butt('B');
 	bb.classList.add(bb.id);
 	bb.setAttribute('data-node', 'b');
@@ -102,7 +94,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	bb.setAttribute('data-parent', 'none');
 	bb.removeAttribute('id');
 	bb.setAttribute('title', 'Bold');
-	bb.addEventListener('click', addTextFormatting);
+	bb.addEventListener('click', (e) => addTextFormatting(e, bb));
 	const ib = butt('i');
 	ib.classList.add(ib.id);
 	ib.setAttribute('data-node', 'i');
@@ -111,7 +103,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	ib.setAttribute('data-parent', 'none');
 	ib.removeAttribute('id');
 	ib.setAttribute('title', 'Italic');
-	ib.addEventListener('click', addTextFormatting);
+	ib.addEventListener('click', (e) => addTextFormatting(e, ib));
 	const so = butt('S');
 	so.classList.add(so.id);
 	so.removeAttribute('id');
@@ -120,7 +112,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	so.setAttribute('data-attr', 'none');
 	so.setAttribute('data-parent', 'none');
 	so.setAttribute('title', 'Strike');
-	so.addEventListener('click', addTextFormatting);
+	so.addEventListener('click', (e) => addTextFormatting(e, so));
 	const ab = butt('a');
 	ab.classList.add(ab.id);
 	ab.removeAttribute('id');
@@ -139,7 +131,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	ok.setAttribute('data-attr', 'href');
 	ok.setAttribute('data-parent', 'none');
 	ab.setAttribute('title', 'Link');
-	ok.addEventListener('click', addTextFormatting);
+	ok.addEventListener('click', (e) => addTextFormatting(e, ok));
 	ab.addEventListener('click', () => {
 		brick.classList.add('visible-url');
 	});
@@ -152,7 +144,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	ol.setAttribute('data-parent', 'ol');
 	ol.firstChild.innerHTML = '1.';
 	ol.setAttribute('title', 'Ordered List');
-	ol.addEventListener('click', addTextFormatting);
+	ol.addEventListener('click', (e) => addTextFormatting(e, ol));
 	const ul = butt('ul');
 	ul.classList.add(ul.id);
 	ul.removeAttribute('id');
@@ -162,7 +154,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	ul.setAttribute('data-parent', 'ul');
 	ul.firstChild.innerHTML = '&#x26AB;';
 	ul.setAttribute('title', 'Unordered List (Bullet Point)');
-	ul.addEventListener('click', addTextFormatting);
+	ul.addEventListener('click', (e) => addTextFormatting(e, ul));
 	const bq = butt('bq');
 	bq.classList.add(bq.id);
 	bq.removeAttribute('id');
@@ -172,7 +164,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	bq.setAttribute('data-parent', 'none');
 	bq.firstChild.innerHTML = '&#x2590;';
 	bq.setAttribute('title', 'Blockquote');
-	bq.addEventListener('click', addTextFormatting);
+	bq.addEventListener('click', (e) => addTextFormatting(e, bq));
 	const colorB = butt('colors');
 	colorB.classList.add(colorB.id);
 	colorB.removeAttribute('id');
@@ -197,12 +189,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 		cb = butt(colors[i][0]);
 		cb.classList.add(cb.id);
 		cb.removeAttribute('id');
-		cb.setAttribute(
-			'title',
-			`${colors[i][0]} ${colors[i][2].charAt(0).toUpperCase()}${colors[
-				i
-			][2].slice(1)}`
-		);
+		cb.setAttribute('title', `${colors[i][0]} ${colors[i][2].charAt(0).toUpperCase()}${colors[i][2].slice(1)}`);
 		cb.classList.add('colors_buttons');
 		cb.firstChild.innerHTML = '';
 		[, cb.style.backgroundColor] = colors[i];
@@ -210,7 +197,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 		cb.setAttribute('data-class', `npf_color_${colors[i][2]}`);
 		cb.setAttribute('data-attr', 'none');
 		cb.setAttribute('data-parent', 'none');
-		cb.addEventListener('click', addTextFormatting);
+		cb.addEventListener('click', (e) => addTextFormatting(e, cb));
 		pbe.appendChild(cb);
 	}
 	const fb = butt('Q');
@@ -221,7 +208,7 @@ const appendRichButtons = (pbe, rich, brick) => {
 	fb.setAttribute('data-class', 'npf_quirky');
 	fb.setAttribute('data-attr', 'none');
 	fb.setAttribute('data-parent', 'none');
-	fb.addEventListener('click', addTextFormatting);
+	fb.addEventListener('click', (e) => addTextFormatting(e, fb));
 	pbe.appendChild(xb);
 	pbe.appendChild(bb);
 	pbe.appendChild(ib);
